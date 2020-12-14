bc6fa95ed4109c5fc0533356c44a24c3a594827d
Aadarsh Singh <aadarshsingh191198@gmail.com>
2020-12-14 09:24:48 +0530

    Add changes for heroku deployment

--- visualiser.py
+++ visualiser.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template, make_response
 from markupsafe import Markup
-import pdfkit
+# import pdfkit
 import pandas as pd
 app = Flask(__name__,
             static_url_path='', 
